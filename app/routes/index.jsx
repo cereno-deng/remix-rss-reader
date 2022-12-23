035c77559435a41a9bd17321b6e261fcf8c647df
Cereno Deng <cereno.deng@gmail.com>
2022-12-24 02:13:44 +0800

    setup Tailwind

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <div>
-      <h1>Remix RSS Reader</h1>
+    <div class="p-20">
+      <h1 className="text-5xl text-center">Remix RSS Reader</h1>
     </div>
   );
 }
